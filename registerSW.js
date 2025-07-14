c120906b3bdac8693e9cefc5509b4415e607408f
Ravi Pundir <onlythirdeye@gmail.com>
2025-07-14 23:51:27 +0530

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/personalize-trading-app/sw.js', { scope: '/personalize-trading-app/' })})}
